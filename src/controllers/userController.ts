f6c05464639af029f498e23fc25475c345c5f030
Louis <you@example.com>
2026-02-23 16:48:49 +0100

    2.4 Gestion centralisée des erreurs supression des try catch

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -3,38 +3,23 @@ import type { Request, Response } from "express";
 import User from "../models/User.js";
 
 export const getAllUsers = async (req: Request, res: Response) => {
-  try {
     const users = await User.findAll();
     res.status(200).json(users);
-  } catch (error) {
-    console.error("Error fetching users:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
 }
 
 export const createUser = async (req: Request, res: Response) => {
-  const { nom, prenom } = req.body;
-  console.log("Received data:", { nom, prenom });
-  try {
+    const { nom, prenom } = req.body;
+    console.log("Received data:", { nom, prenom });
     const newUser = await User.create({ nom, prenom });
     res.status(201).json(newUser);
-  } catch (error) {
-    console.error("Error creating user:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
 }
 
 export const deleteUser = async (req: Request, res: Response) => {
-  const userId = req.params.id;
-  try {
+    const userId = req.params.id;
     const deletedUser = await User.destroy({ where: { id: userId } });
     if (deletedUser) {
       res.status(200).json({ message: "User deleted successfully" });
     } else {
       res.status(404).json({ message: "User not found" });
     }
-  } catch (error) {
-    console.error("Error deleting user:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
 }
