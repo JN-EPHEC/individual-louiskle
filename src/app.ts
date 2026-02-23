0a643b6d06e418b0f717b175961c86972897ec5d
Louis <you@example.com>
2026-02-23 16:38:15 +0100

    2.3.1 Création dun Logger

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,12 @@ import type { Request, Response } from "express";
 import userRoutes from "./routes/userRoutes.js";
 import sequelize from "./config/database.js";
 import Userfgdf from "./models/User.js";
+import { requestLogger } from './middlewares/logger.js'
 
 const app = express();
 const PORT = 3000;
 
+app.use(requestLogger);
 app.use(express.json());
 
 app.use(express.static("public"));
